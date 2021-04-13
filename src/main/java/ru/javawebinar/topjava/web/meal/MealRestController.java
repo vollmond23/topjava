472101d00262edec86133966804dc52b26998c63
Golovitskiy Mikhail <m.golovitskiy@gmail.com>
2021-04-13 22:06:06 +0300

    Task 5: Meal injection

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.web.meal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.to.MealTo;
@@ -12,6 +13,7 @@ import java.util.List;
 
 import static ru.javawebinar.topjava.web.SecurityUtil.authUserId;
 
+@Controller
 public class MealRestController {
     protected final Logger log = LoggerFactory.getLogger(getClass());
 
